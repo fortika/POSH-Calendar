fa68fc125c0d3fe48704a756f676d161f2e0c2cf
Fortika <Fortika@users.noreply.github.com>
2017-07-14 19:04:20 +0200

    Get-CalendarItem

---
+++ Get-CalendarItem.ps1
@@ -0,0 +1,34 @@
+<#
+	.SYNOPSIS
+
+	.DESCRIPTION
+
+	.PARAMETER  Subject
+
+	.EXAMPLE
+
+	.NOTES
+
+	.LINK
+
+#>
+
+Function Get-CalendarItem {
+#http://blogs.technet.com/b/heyscriptingguy/archive/2009/01/29/how-can-i-display-my-office-outlook-appointments-without-starting-office-outlook.aspx
+#http://blogs.technet.com/b/heyscriptingguy/archive/2011/05/24/use-powershell-to-export-outlook-calendar-information.aspx
+    [cmdletBinding(SupportsShouldProcess=$true,
+                   ConfirmImpact='Medium')]
+    Param(
+    )
+
+    BEGIN {
+    }
+
+    PROCESS {
+    }
+
+    END {
+    }
+
+}
+
